418ba8dd5c7152633239e7236c1e951833f1d142
Jou <joaryandrian@gmail.com>
2022-02-05 03:50:41 +0300

    Jou | create modeles and config the administration pannel

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,12 @@ class User extends \TCG\Voyager\Models\User
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Get the posts for the user.
+     */
+    public function posts()
+    {
+        return $this->hasMany('App\Models\Post');
+    }
 }
